11151497e4e3dac4e7f2f68103cceca38b8d0fa7
Scott Oseychik <scottos@microsoft.com>
2019-06-04 17:20:27 -0400

    latest changes

--- Check-License.ps1
+++ Check-License.ps1
@@ -1,43 +1,56 @@
+# .SYNOPSIS
+#  Diagnose why a given mailbox fails License validation
+#
+#  Copyright (c) 2019 Microsoft Corporation. All rights reserved.
+#
+#  THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE RISK
+#  OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$MailboxGuid,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Organization
+    )
+
 # Globals
 $ErrorActionPreference = "SilentlyContinue";
 
-# Variables
-$exchangeGuid = "3ba2b1c0-77e5-47c9-b4b9-4f2472418d2e"
-$org = "expediacorp.onmicrosoft.com"
-
 # Commands
 Write-Progress "Getting Organization properties";
-$o = Get-Organization $org;
+$o = Get-Organization $Organization;
+
 Write-Progress "Getting Mailbox properties";
-$m = Get-Mailbox $exchangeGuid -Organization $o;
+$m = Get-Mailbox $MailboxGuid -Organization $o;
 
 # Is this a UserMailbox? Otherwise, we don't enforce licensing
 [bool] $boolIsUserMailbox = $false;
 $boolIsUserMailbox = ($m.RecipientTypeDetails -contains 'UserMailbox');
 if($boolIsUserMailbox -eq $true){
-    Write-Host "UserMailbox is" $boolIsUserMailbox;
+    Write-Host "SUCCESS: UserMailbox is" $boolIsUserMailbox;
 }
 else{
-    Write-Host "Not a UserMailbox; license not enforced";
+    Write-Host "Not a UserMailbox (licensing is not enforced on Shared, Room, and Resource mailboxes)";
 }
 
 # Validate IsLicensingEnforced at org level
 $boolIsLicensingEnforced = $o.IsLicensingEnforced;
 if($boolIsLicensingEnforced -eq $true){
-    Write-Host "IsLicensingEnforced is" $boolIsLicensingEnforced;
+    Write-Host "SUCCESS: IsLicensingEnforced is" $boolIsLicensingEnforced;
 }
 else{
-    Write-Host "IsLicensingEnforced is" $boolIsLicensingEnforced;
+    Write-Host "IsLicensingEnforced is" $boolIsLicensingEnforced "(Licensing is not being enforced within this Tenant)";
 }
 
 # Evaluate PersistedCapabilities
 [bool] $boolPersistedCapabilities = $false;
-$boolPersistedCapabilities = ($m.PersistedCapabilities -contains 'BPOS_S_Enterprise');
+$boolPersistedCapabilities = (($m.PersistedCapabilities -contains 'BPOS_S_Standard') -or ($m.PersistedCapabilities -contains 'BPOS_S_Enterprise'));
 if($boolPersistedCapabilities -eq $true){
-    Write-Host "PersistedCapabilities is" $boolPersistedCapabilities;
+    Write-Host "SUCCESS: PersistedCapabilities is" $boolPersistedCapabilities;
 }
 else{
-    Write-Warning "PersistedCapabilities is null";
+    Write-Warning "PersistedCapabilities is null (a Mailbox Plan has not been assigned to this Mailbox)";
 }
 
 # Evaluate SkuAssigned
@@ -47,7 +60,7 @@ if($boolSkuAssigned -eq $true){
     Write-Host "SkuAssigned is" $boolSkuAssigned;
 }
 else{
-    Write-Warning "SkuAssigned is null";
+    Write-Warning "SkuAssigned is null (a license has not been assiged to this mailbox)";
 }
 
 # Evaluate WithinGracePeriod (Grace period == MailboxCreated < 30 days)
@@ -56,5 +69,5 @@ if($boolWithinGracePeriod -eq $true){
     Write-Host "WithinGracePeriod is" $boolWithinGracePeriod;
 }
 else{
-    Write-Warning "Mailbox not within Grace Period";
+    Write-Warning "Mailbox not within Grace Period (Mailbox was created > 30 days ago)";
 }
